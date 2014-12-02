e5e5c28d9102fb86f762bf907f0cd39123e4b27f
Passarinho <passarinho4@gmail.com>
2014-12-02 19:22:03 +0100

    TMS-42 Add files and classes.

--- app/models/users/UserImpl.scala
+++ app/models/users/UserImpl.scala
@@ -10,8 +10,6 @@ import reactivemongo.bson.BSONObjectID
  */
 class UserImpl(val _id:Option[BSONObjectID],
                 val personalData: UserProperties,
-                val tournaments: List[Tournament],
-                val teams: List[Team],
                 val statistics: Statistics) extends User
 {
 
