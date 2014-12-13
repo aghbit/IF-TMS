1151489338fc060dcd26d10a2cb2827c489be37e
ludwik <ludwik0@onet.eu>
2014-12-13 15:24:45 +0100

    TournamentStrategy part

---
+++ app/models/strategy/Game/package.scala
@@ -0,0 +1,16 @@
+package models.strategy
+
+
+/**
+ * Created by ludwik on 13.12.14.
+ */
+package object Tree {
+   class Game(var right : Game, var left:Game,var parent:Game, var value:Match)
+   class EliminationTree(val root: Game){
+    def this(){
+       this(new Game(null,null,null,null))
+    }
+  //  def getLatest():Game = {return new Game();}
+  }
+}
+
