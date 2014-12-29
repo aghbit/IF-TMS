8b26eca8798470da325183aa0cb18ee811901aec
wojciech <wojciech.adaszynski@gmail.com>
2014-12-29 16:09:00 +0100

    Changes in angular structute, angular router and some small fixes

--- public/javascripts/controllers/StatisticsController.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by wojciech on 13.12.14.
- */
-var myApp = angular.module("myApp", []);
-myApp.controller('HelloController', function ($scope) {
-    $scope.testtest = "<b>ssdsda</b>";
-});
---
+++ public/userapp/controllers/StatisticsController.js
@@ -0,0 +1,6 @@
+
+
+mainApp.controller('StatisticsController', ['$scope', function($scope) {
+    $scope.testmessage = "testing message";
+
+}]);
