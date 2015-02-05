1033e9e5815a1a0b135155ae80e97209a6d349a2
wojciech <wojciech.adaszynski@gmail.com>
2015-02-05 23:14:34 +0100

    Authentication

--- public/userapp/app.js
+++ public/userapp/app.js
@@ -1,4 +1,4 @@
-var mainApp = angular.module('mainApp', ['ui.router', 'angular-loading-bar']);
+var mainApp = angular.module('mainApp', ['ui.router', 'angular-loading-bar','ngDialog']);
 mainApp.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise("/");
@@ -20,11 +20,12 @@ mainApp.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl: "/assets/userapp/partials/tournaments/main.html",
             controller: 'TournamentsController'
         })
-        .state('register', {
-            url: "/register",
-            templateUrl: "/assets/userapp/partials/register/register.html",
-            controller: 'RegisterController'
-        })
+        //NOT USED
+        //.state('register', {
+        //    url: "/register",
+        //    templateUrl: "/assets/userapp/partials/register/register.html",
+        //    controller: 'RegisterController'
+        //})
         .state('login', {
             url: "/login",
             templateUrl: "/assets/userapp/partials/login/login.html",
@@ -33,3 +34,51 @@ mainApp.config(function ($stateProvider, $urlRouterProvider) {
 
 });
 
+mainApp.factory('SessionService', function() {
+    var usersCredentials = {
+        token : '12',
+        isLoggedIn : false,
+        login : function () {
+            ngDialog.open({
+                template: 'templateId',
+                className: 'ngdialog-theme-plain',
+                closeByDocument: false
+            });
+        }
+    };
+    return usersCredentials;
+});
+
+//token always in headers
+mainApp.factory('sessionInjector', ['SessionService', function(SessionService) {
+    var sessionInjector = {
+        request: function(config) {
+            config.headers['session-token'] = SessionService.token;
+
+            return config;
+        }
+    };
+    return sessionInjector;
+}]);
+mainApp.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('sessionInjector');
+}]);
+
+//always check if user is authorized
+var httpInterceptor = function ($provide, $httpProvider) {
+    $provide.factory('httpInterceptor',function ($q, $window, $location) {
+        return {
+            response: function (response) {
+                return response || $q.when(response);
+            },
+            responseError: function (rejection) {
+                if(rejection.status === 401) {
+                    $location.url('login');
+                }
+                return $q.reject(rejection);
+            }
+        };
+    });
+    $httpProvider.interceptors.push('httpInterceptor');
+};
+mainApp.config(httpInterceptor);
