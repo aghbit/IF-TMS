7643a27889ec707593ad272634888a64da2fe326
ludwik <ludwik0@onet.eu>
2015-02-15 18:13:41 +0100

    Nulle poprawione na None

--- test/it/GenerateTreeIT.scala
+++ test/it/GenerateTreeIT.scala
@@ -13,83 +13,83 @@ import org.scalatest.junit.JUnitRunner
  */
 @RunWith(classOf[JUnitRunner])
 class GenerateTreeIT extends FunSuite with BeforeAndAfter with MockitoSugar {
-//  var underTest: TournamentStrategy = _
-//
-//  before {
-//
-//  }
-//  test("generateTree: Simple integration test 1") {
-//
-//    //given
-//    var my_list = List(mock[Team])
-//    for(i <- 0 until 14) my_list = mock[Team]::my_list
-//    underTest = new SingleEliminationStrategy()
-//    val expectedDeph = 4
-//    val tree = underTest.generateTree(my_list)
-//
-//    //when
-//
-//    var count: Int = 0
-//    var tmp = tree.root
-//    while(tmp!=null){
-//      count = count +1
-//      tmp = tmp.left
-//    }
-//
-//    //then
-//
-//    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
-//
-//  }
-//
-//  test("generateTree: Simple integration test 2") {
-//
-//    //given
-//
-//    var my_list = List(mock[Team])
-//    for(i <- 0 until 20) my_list = mock[Team]::my_list
-//    underTest = new SingleEliminationStrategy()
-//    val expectedDeph = 5
-//    val tree = underTest.generateTree(my_list)
-//
-//    //when
-//
-//    var count: Int = 0
-//    var tmp = tree.root
-//    while(tmp!=null){
-//      count = count +1
-//      tmp = tmp.left
-//    }
-//
-//    //then
-//
-//    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
-//
-//  }
-//  test("generateTree: Simple integration test 3") {
-//
-//    //given
-//
-//    var my_list = List(mock[Team])
-//    underTest = new SingleEliminationStrategy()
-//    val expectedDeph = 1
-//    val tree = underTest.generateTree(my_list)
-//
-//    //when
-//
-//    var count: Int = 0
-//    var tmp = tree.root
-//    while(tmp!=null){
-//      count = count +1
-//      tmp = tmp.left
-//    }
-//
-//    //then
-//
-//    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
-//
-//  }
-//
+  var underTest: TournamentStrategy = _
+
+  before {
+
+  }
+  test("generateTree: Simple integration test 1") {
+
+    //given
+    var my_list = List(mock[Team])
+    for(i <- 0 until 14) my_list = mock[Team]::my_list
+    underTest = new SingleEliminationStrategy()
+    val expectedDeph = 4
+    val tree = underTest.generateTree(my_list)
+
+    //when
+
+    var count: Int = 1
+    var tmp = tree.root
+    while(tmp.left!=None){
+      count = count +1
+      tmp = tmp.left.get
+    }
+
+    //then
+
+    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
+
+  }
+
+  test("generateTree: Simple integration test 2") {
+
+    //given
+
+    var my_list = List(mock[Team])
+    for(i <- 0 until 20) my_list = mock[Team]::my_list
+    underTest = new SingleEliminationStrategy()
+    val expectedDeph = 5
+    val tree = underTest.generateTree(my_list)
+
+    //when
+
+    var count: Int = 1
+    var tmp = tree.root
+    while(tmp.left!=None){
+      count = count +1
+      tmp = tmp.left.get
+    }
+
+    //then
+
+    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
+
+  }
+  test("generateTree: Simple integration test 3") {
+
+    //given
+
+    var my_list = List(mock[Team])
+    underTest = new SingleEliminationStrategy()
+    val expectedDeph = 1
+    val tree = underTest.generateTree(my_list)
+
+    //when
+
+    var count: Int = 1
+    var tmp = tree.root
+    while(tmp.left!=None){
+      count = count +1
+      tmp = tmp.left.get
+    }
+
+    //then
+
+    assert(count==expectedDeph, "Genereting integrated treee doesnt work.")
+
+  }
+
 
 
 
