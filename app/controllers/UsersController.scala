8da2dfe9936532571b69b1420161c549297aeaec
Passarinho <passarinho4@gmail.com>
2015-02-23 14:09:38 +0100

    UserImpl to User..

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -14,6 +14,7 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
+
 object UsersController extends Controller{
 
   private val repository = new UserRepository()
--- app/repositories/UserRepository.scala
+++ app/repositories/UserRepository.scala
@@ -8,13 +8,13 @@ import org.springframework.data.mongodb.core.query.{Criteria, Query}
 /**
  * Created by Szymek.
  */
-class UserRepository extends Repository[UserImpl] {
+class UserRepository extends Repository[User] {
 
   override val collectionName: String = "Users"
-  override val clazz: Class[UserImpl] = classOf[UserImpl]
+  override val clazz: Class[User] = classOf[User]
 
   override def insert[T](obj: T): Unit = {
-    val user = obj.asInstanceOf[UserImpl]
+    val user = obj.asInstanceOf[User]
     val query = new Query(Criteria where "personalData.login" is user.getProperties.login)
     val usersList = super.find(query)
     if(!usersList.isEmpty) {
