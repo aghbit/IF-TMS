7064727b0a54aeb3580d6299c3280423f6f76a76
Passarinho <passarinho4@gmail.com>
2015-02-24 22:14:59 +0100

    Tournament Form.

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,7 @@
 (function($){
   $(function(){
-    $('.datepicker').pickadate();
     $('.button-collapse').sideNav();
     $('.dropdown-button').dropdown({hover: false});
     $('.slider').slider({full_width: true});
-
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- public/userapp/controllers/TournamentsController.js
+++ public/userapp/controllers/TournamentsController.js
@@ -1,4 +1,12 @@
 mainApp.controller('TournamentsController', ['$scope', function ($scope) {
+    //important! it loads js for datePicker.
+    angular.element(document).ready(function(){
+        $('.datepicker').pickadate();
+        $('.timepicker').pickatime({
+            min:[6,00],
+            max:[23,00]
+        })
+    })
     $scope.testmessage = "Tournaments page";
 
 }]);
