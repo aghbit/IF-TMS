85d41e54701b381dbb5ba63916acfed341a442d9
Piotr Bochenek <p-bochenek@outlook.com>
2015-04-03 16:34:47 +0200

    Navbar differs

--- public/userapp/controllers/MainController.js
+++ public/userapp/controllers/MainController.js
@@ -1,7 +1,15 @@
 mainApp.controller('MainController', ['$scope', 'ngDialog','SessionService','$location', function($scope, ngDialog,SessionService,$location) {
+
+    $scope.loggedIn = SessionService.isLoggedIn;
+
+    $scope.$on('LOGIN_EVENT', function(event) {
+        $scope.loggedIn = SessionService.isLoggedIn;
+    });
+
     $scope.signOut = function(){
         SessionService.token = " ";
         SessionService.isLoggedIn = false;
-        $location.url("")
+        $location.url("");
+        $scope.loggedIn = SessionService.isLoggedIn;
     }
 }]);
