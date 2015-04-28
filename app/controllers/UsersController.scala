da43238a19185b2d33a2b4db5f6aa91443730e4a
Passarinho <passarinho4@gmail.com>
2015-04-28 18:27:34 +0200

    UserController

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -8,9 +8,11 @@ import models.user.userproperties.UserProperties
 import models.user.users.userimpl.UserImpl
 import org.springframework.data.mongodb.core.query.{Criteria, Query}
 import play.api.libs.json.JsError
+import play.api.libs.json.Reads._
 import play.api.mvc.{Action, Controller}
 import reactivemongo.bson.BSONObjectID
 import repositories.UserRepository
+import play.api.libs.functional.syntax._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -31,8 +33,8 @@ object UsersController extends Controller{
             repository.insert(user)
             Future.successful(Created)
           } catch {
-            case e:UserWithThisLoginExistsInDB => Future.failed(new Exception("DUPA1"))
-            case e:Exception => Future.failed(new Exception("DUPA"))
+            case e:UserWithThisLoginExistsInDB => Future.failed(e)
+            case e:Exception => Future.failed(e)
           }
         case Left(e) => Future.successful(BadRequest("Detected error: " + JsError.toFlatJson(e)))
       }
@@ -40,31 +42,49 @@ object UsersController extends Controller{
 
   def getUser(id: String) = AuthorizationAction.async {
     request => {
-      val token = TokenImpl(request.headers.get("token").get)
-      val userID = token.getUserID
-      if(userID.stringify == id){
-        val query = new Query(Criteria where "_id" is BSONObjectID(id))
-        val users = repository.find(query)
-        Future.successful(Ok(users.get(ListEnum.head).toJson))
-      }else {
-        Future.successful(Unauthorized("You are not authorized to see this content!"))
+      request.headers.get("token") match {
+        case Some(s) =>
+          val token = TokenImpl(s)
+          val userID = token.getUserID
+          userID.stringify match {
+            case `id` =>
+              val query = new Query(Criteria where "_id" is BSONObjectID(id))
+              val users = repository.find(query)
+              Future.successful(Ok(users.get(ListEnum.head).toJson))
+            case _ =>
+              Future.successful(Unauthorized("You are not authorized to see this content!"))
+          }
+          //This should never execute, because AuthorizationAction checked the token.
+        case None => Future.successful(NotFound("User with this ID was not found!"))
       }
     }
   }
 
   def login() = Action.async(parse.json) {
     request =>
-
-      val login = request.body.\("login").validate[String].get
-      val password = request.body.\("password").validate[String].get
-      val query = new Query(Criteria where "personalData.login" is login and "personalData.password" is password)
-      val users = repository.find(query)
-      if(users.isEmpty){
-        Future.successful(Unauthorized("Wrong login or password!"))
-      }else{
-        val token = users.get(ListEnum.head).generateToken
-        TokensKeeper.addToken(token)
-        Future.successful(Ok(token.toString))
+      val login = request.body.\("login").validate[String](minLength[String](5)
+                                                          andKeep maxLength[String](20)).asEither
+      val password = request.body.\("password").validate[String](minLength[String](5)
+                                                                andKeep maxLength[String](20)).asEither
+      (login, password) match {
+        case(Right(l), Right(p)) =>
+          val query = new Query(Criteria where "personalData.login" is l and
+            "personalData.password" is p)
+          val users = repository.find(query)
+          if(users.isEmpty){
+            Future.successful(Unauthorized("Wrong login or password!"))
+          }else{
+            val token = users.get(ListEnum.head).generateToken
+            TokensKeeper.addToken(token)
+            Future.successful(Ok(token.toString))
+          }
+        case (Right(l), Left(p)) =>
+          Future.successful(BadRequest("Detected error: " + JsError.toFlatJson(p)))
+        case (Left(l), Right(p)) =>
+          Future.successful(BadRequest("Detected error: " + JsError.toFlatJson(l)))
+        case (Left(l), Left(p)) =>
+          Future.successful(BadRequest("Detected error: " + JsError.toFlatJson(l) + " "
+                                        + JsError.toFlatJson(p)))
       }
   }
 
