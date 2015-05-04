b47a8d09b4c87905294f1b610c62e26290a5cba6
ludwik <ludwik0@onet.eu>
2015-05-04 02:43:53 +0200

    Added popUps

--- public/userapp/controllers/LoginController.js
+++ public/userapp/controllers/LoginController.js
@@ -3,8 +3,8 @@
  */
 mainApp.controller('LoginController', ['$scope','$http','$location','ngDialog','SessionService', function($scope,$http,$location, ngDialog, SessionService) {
     $scope.credentials = {
-        login : "Passarinho",
-        password : "Passarinho123"
+        login : "Bucq",
+        password : "idefix1."
     };
     $scope.submit = function(){
         $http.post('/api/login', {"login" : $scope.credentials.login, "password": $scope.credentials.password}).
