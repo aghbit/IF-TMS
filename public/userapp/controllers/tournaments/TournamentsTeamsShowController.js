01b700f5e450eac18379f747296ec9d6bf768365
ludwik <ludwik0@onet.eu>
2015-05-07 17:09:51 +0200

    Parsing Teams to CSV file

--- public/userapp/controllers/tournaments/TournamentsTeamsShowController.js
+++ public/userapp/controllers/tournaments/TournamentsTeamsShowController.js
@@ -1,26 +1,57 @@
 /**
  * Created by szymek on 08.03.15.
  */
+mainApp.config(['$compileProvider',
+    function ($compileProvider) {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/);
+    }]);
 mainApp.controller('TournamentsTeamsShowController', ['$scope', '$location', '$http', '$stateParams', 'SessionService',
     function ($scope, $location, $http, $stateParams, SessionService) {
-    $http.get('api/tournaments/' + $stateParams.id, {}).
-        success(function(data, status, headers, config) {
-            $scope.tournament = data;
-        }).error(function(data, status, headers, config, statusText) {
+        $scope.getTournament = function(){
+            $http.get('api/tournaments/' + $stateParams.id, {}).
+                success(function(data, status, headers, config) {
+                    $scope.tournament = data;
+                }).error(function(data, status, headers, config, statusText) {
 
-        });
-    $http.get('api/tournaments/' + $stateParams.id+"/teams", {}).
-        success(function(data, status, headers, config) {
-            $scope.teams = data;
-            $('.collapsible').collapsible({
-                accordion : false
-            });
+                });
+        };
+        $scope.getTeams = function() {
+            $http.get('api/tournaments/' + $stateParams.id + "/teams", {}).
+                success(function (data, status, headers, config) {
+                    $scope.teams = data;
+                    $('.collapsible').collapsible({
+                        accordion: false
+                    });
+                }).error(function (data, status, headers, config, statusText) {
 
-        }).error(function(data, status, headers, config, statusText) {
-        });
+                });
+        };
+        $scope.getTournament();
+        $scope.getTeams();
+        $scope.generateCSV = function(){
+        $scope.toParse = $scope.getTournament();
+        var document = "";
+        for(var i=0;i<$scope.teams.length;i++){
+            var obj = $scope.teams[i];
+            var teamName = obj.name;
+            document = document+ teamName+",";
+            document = document + obj.captain.name+ ","+obj.captain.surname+",";
+            document = document + obj.captain.phone+",";
+            document = document + obj.captain.mail+",";
+            for(var j=0;j<obj.players.length-1;j++){
+                document +=obj.players[j].name+ ","+ obj.players[j].surname+",";
+            }
+            document +=obj.players[obj.players.length-1].name+ ","+ obj.players[obj.players.length-1].surname+"\n";
+        }
+        //To share file to a client
+        var blob = new Blob([ document ], { type : 'text/plain' });
+        $scope.url = (window.URL || window.webkitURL).createObjectURL( blob );
+    };
+
 
     $scope.addAnotherPlayer = function(teamID){
         $location.path('/teams/' + teamID + '/addPlayer');
-    }
+    };
+
 
 }]);
