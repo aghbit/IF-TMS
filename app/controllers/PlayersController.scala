af071fc822ba5b5be6ec4e1d7979fa3a4c1eddb8
Passarinho <passarinho4@gmail.com>
2015-05-08 00:46:55 +0200

    Controllers refactor.

--- app/controllers/PlayersController.scala
+++ app/controllers/PlayersController.scala
@@ -5,6 +5,7 @@ import models.enums.ListEnum
 import models.exceptions.TooManyMembersInTeamException
 import models.player.players.DefaultPlayerImpl
 import org.springframework.data.mongodb.core.query.{Criteria, Query}
+import play.api.libs.json.JsError
 import play.api.mvc.{Action, Controller}
 import reactivemongo.bson.BSONObjectID
 
@@ -20,19 +21,30 @@ object PlayersController extends Controller {
 
   def createPlayer(teamId: String) = Action.async(parse.json) {
     request =>
-      val query = new Query(Criteria where "_id" is BSONObjectID(teamId))
-      val team = teamRepository.find(query).get(ListEnum.head)
-      val name = request.body.\("name").validate[String](minLength(5)).get
-      val surname = request.body.\("surname").validate[String](minLength(5)).get
-      val player = DefaultPlayerImpl(name, surname)
-      try {
-        team.addPlayer(player)
-        playerRepository.insert(player)
-        teamRepository.insert(team)
-        Future.successful(Ok("Player added!"))
-      }catch {
-        case e:TooManyMembersInTeamException => Future.failed(e)
-        case e:Throwable => Future.failed(e)
+      val name = request.body.\("name").validate[String](minLength(5) andKeep maxLength(20)).asEither
+      val surname = request.body.\("surname").validate[String](minLength(5) andKeep maxLength(20)).asEither
+
+      val mapEither = Map(("name", name), ("surname", surname))
+      val inputsRight = mapEither.collect{case (s:String, Right(i)) => (s, i)}
+      val inputErrors = mapEither.collect{case (s:String, Left(e)) => e}
+      if(inputErrors.isEmpty){
+        val query = new Query(Criteria where "_id" is BSONObjectID(teamId))
+        val team = teamRepository.find(query).get(ListEnum.head)
+        val player = DefaultPlayerImpl(inputsRight.getOrElse("name", ""), inputsRight.getOrElse("surname", ""))
+        try {
+          team.addPlayer(player)
+          playerRepository.insert(player)
+          teamRepository.insert(team)
+          Future.successful(Ok("Player added!"))
+        }catch {
+          case e:TooManyMembersInTeamException => Future.failed(e)
+          case e:Throwable => Future.failed(e)
+        }
+      }else {
+        val JsErrors = inputErrors.map(e => JsError.toFlatJson(e))
+        Future.successful(BadRequest("Detected error: " + JsErrors))
       }
+
+
   }
 }
