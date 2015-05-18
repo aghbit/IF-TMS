54318da4c1df05617b9ae0e7f60d62c933c1f04e
Passarinho <passarinho4@gmail.com>
2015-05-18 17:00:20 +0200

    Merge branches 'TMS-86-refactor-controller-v2' and 'master' of https://bitbucket.org/aghbit/if-tms into TMS-86-refactor-controller-v2
    Conflicts:
    	public/userapp/controllers/tournaments/TournamentsEnrollmentController.js

--- app/configuration/MongoDBConfiguration.scala
+++ app/configuration/MongoDBConfiguration.scala
@@ -15,8 +15,8 @@ class MongoDBConfiguration(var databaseName: String) extends AbstractMongoConfig
     if(URI == null) {
       new MongoClient("127.0.0.1", 27017)
     } else {
-      databaseName = "tms-production"
-      new MongoClient(new MongoClientURI("mongodb://tms:RjNuZ1BlNkpWeUttWEs3V0RmSm1HWHJoaGlVeHZpeER5WVJaYjlIQjBPTT0K@172.17.0.113:27017/tms-production"))
+      databaseName = "tms_db"
+      new MongoClient(new MongoClientURI("mongodb://tms:tms123@ds061928.mongolab.com:61928/tms_db"))
     }
   }
 }
