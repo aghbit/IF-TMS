cb3a4887a46447a99dd69fc5db51e03d665b8152
Passarinho <passarinho4@gmail.com>
2015-05-18 16:13:54 +0200

    Merge branches 'TMS-86-refactor-controller-v2' and 'master' of https://bitbucket.org/aghbit/if-tms into TMS-86-refactor-controller-v2
    Conflicts:
    	public/userapp/controllers/tournaments/TournamentsEnrollmentController.js

--- app/configuration/MongoDBConfiguration.scala
+++ app/configuration/MongoDBConfiguration.scala
@@ -8,8 +8,14 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration
  */
 
 class MongoDBConfiguration(val databaseName: String) extends AbstractMongoConfiguration {
-
   override def getDatabaseName: String = databaseName
 
-  override def mongo(): Mongo = new MongoClient("127.0.0.1", 27017)
+  override def mongo(): Mongo = {
+    val URI = System.getenv("MONGO_URI")
+    if(URI == null) {
+      new MongoClient("127.0.0.1", 27017)
+    } else {
+      new MongoClient(URI)
+    }
+  }
 }
