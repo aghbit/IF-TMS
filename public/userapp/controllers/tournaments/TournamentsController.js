3644d923c5ee43fe3228331b628c49388ba2fc74
ravinha <robbie16@vp.pl>
2015-06-01 16:01:50 +0200

    Rotate arrow added

--- public/userapp/controllers/tournaments/TournamentsController.js
+++ public/userapp/controllers/tournaments/TournamentsController.js
@@ -8,5 +8,18 @@ mainApp.controller('TournamentsController', ['$scope',  '$http', function ($scop
         }).error(function(data, status, headers, config, statusText) {
             notification("Error.", 4000, false)
         });
+    $scope.openedTournamentItem = undefined;
+    $scope.rotateArrow = function(id){
+        $("#list-icon"+id).toggleClass("rotate-clockwise");
+        if($scope.openedTournamentItem !== undefined && $scope.openedTournamentItem!== id){
+            $("#list-icon"+$scope.openedTournamentItem).toggleClass("rotate-clockwise");
+        }
+        if($scope.openedTournamentItem === id){
+            $scope.openedTournamentItem = undefined;
+        }
+        else{
+            $scope.openedTournamentItem = id;
+        }
+    }
 
 }]);
