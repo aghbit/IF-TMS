7327358482d339ec420c13e26107f282b373c2b4
Maciej Kocot <mkocot@op.pl>
2016-02-06 23:12:21 +0100

    mytournaments, images (finally)

--- public/userapp/controllers/tournaments/TournamentsDebugController.js
+++ public/userapp/controllers/tournaments/TournamentsDebugController.js
@@ -5,10 +5,17 @@
 mainApp.controller('TournamentsDebugController', ['$scope', '$location', '$http', '$stateParams', 'SessionService', 'ngDialog',
     function ($scope, $location, $http, $stateParams, SessionService, ngDialog) {
 
+        $http.get('api/tournaments/'+$stateParams.id, {}).
+            success(function(data, status, headers, config) {
+                $scope.tournamentName = data.properties.description.name;
+            }).error(function(data, status, headers, config, statusText) {
+                notification("Sorry. An error occurred while loading tournament info.", 4000, false);
+            });
+
         $scope.show = function(type) {
             $http.get('/api/tournaments/' + $stateParams.id + "/"+type).
                 success(function(data, status, headers, config) {
-                    $scope.message = data;
+                    $scope.message = JSON.stringify(data, null, 4);
                 }).error(function(data, status, headers, config, statusText) {
                     $scope.message = "";
                     alert(status + " " + data)
